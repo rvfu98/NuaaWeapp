4d3b03c4359eb40cc0508c4a7132c1b9f9b59afa
rvfu98 <rvfu98@gmail.com>
2017-09-22 22:50:14 +0800

    简单地完成了教务通知板块的开发

--- pages/notice/index.js
+++ pages/notice/index.js
@@ -6,17 +6,19 @@ Page({
     onTap: function(e){
         //console.log(e.currentTarget.dataset.url)
         wx.navigateTo({
-            url: '../noticeDetail/index?url=' + e.currentTarget.dataset.url
+            url: '../noticeDetail/index?url=' + e.currentTarget.dataset.url + '&title=' + e.currentTarget.dataset.title
         })
     },
     onLoad: function(){
         var that = this;
+        wx.showNavigationBarLoading();
         wx.request({
             url: 'https://proj.rvfu98.com/nuaaweapp/notice.php',
             success: function (e) {
                 that.setData({
                     notices: e.data
                 })
+                wx.hideNavigationBarLoading();
             }
         })
         
--- pages/noticeDetail/index.js
+++ pages/noticeDetail/index.js
@@ -5,8 +5,12 @@ Page({
 
     },
     onLoad: function(options){
+        wx.showNavigationBarLoading();
         var that = this;
         this.data.url = options.url;
+        this.setData({
+            title: options.title
+        })
         wx.request({
             url: 'https://proj.rvfu98.com/nuaaweapp/noticeDetail.php',
             method: 'post',
@@ -17,9 +21,14 @@ Page({
                 url: options.url
             },
             success : function(e){
-              
-
                 WxParse.wxParse('article', 'html', e.data, that, 5);
+                wx.hideNavigationBarLoading();
+            },
+            fail: function(e){
+                wx.hideNavigationBarLoading();
+                wx.showToast({
+                    title: '发生了一些错误',
+                })
             }
         })
     },
@@ -31,7 +40,7 @@ Page({
                 wx.getClipboardData({
                     success: function (res) {
                         wx.showToast({
-                            title: '本文链接已经复制到剪切板',
+                            title: '本文链接已复制',
                         })
                     }
                 })
