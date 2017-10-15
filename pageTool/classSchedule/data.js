cf2c01dc850c9bb91a9f3f8dad94c4fca2cba2dc
rvfu98 <rvfu98@gmail.com>
2017-10-15 16:56:13 +0800

    添加了班级课表的功能

---
+++ pageTool/classSchedule/data.js
@@ -0,0 +1,66 @@
+var data = {
+    SEMESTERS: [
+        {
+            show_name: '2017-18 学期1',
+            name: '2017-2018,1',
+            begin_day: '2017/9/4'
+        }, {
+            show_name: '2016-17 学期2',
+            name: '2016-2017,2',
+            begin_day: '2017/2/13'
+        }, {
+            show_name: '2016-17 学期1',
+            name: '2016-2017,1',
+            begin_day: '2017/8/29'
+        }, {
+            show_name: '2015-16 学期2',
+            name: '2015-2016,2',
+            begin_day: '2017/2/22'
+        }, {
+            show_name: '2015-16 学期1',
+            name: '2015-2016,1',
+            begin_day: '2017/8/31'
+        }, {
+            show_name: '2014-15 学期2',
+            name: '2014-2015,2',
+            begin_day: '2017/3/2'
+        }, {
+            show_name: '2014-15 学期1',
+            name: '2014-2015,1',
+            begin_day: '2017/9/1'
+        }
+    ],
+    SEMESTER: 0,
+    WEEKS: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    DAYS: ['一', '二', '三', '四', '五', '六', '日'],
+    UNITS: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    COLORS: [
+        '5addf1',
+        '5aed56',
+        'b2ef4a',
+        'ead94b',
+        'f3bc1b',
+        'c2b4f2',
+        '34ebe7',
+        'ea8200',
+        'f76359',
+        '6f58f0',
+        '37b4ec',
+        '81cef8',
+        'aabdff'
+    ],
+    dates: [],
+    days: [],
+    units: [],
+    maxDay: null,
+    maxUnit: null,
+    semester: null,
+    week: null,
+    month: null,
+    date: null,
+    day: null,
+    courses: [],
+    studentid: null
+}
+
+module.exports = data;
